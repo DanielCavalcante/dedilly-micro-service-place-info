61a5c54a1b41ebbaf6a92c881e0280428d274221
Daniel Cavalcante de Souza <danielcdesouza@gmail.com>
2023-05-07 14:20:44 -0300

    refactor: 💡 Add geo code url to enviroment

--- src/infra/services/geocode/index.ts
+++ src/infra/services/geocode/index.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const geoCodeBaseUrl = axios.create({
-  baseURL: "https://geocode.maps.co/",
+  baseURL: process.env.GEO_CODE_BASE_URL ?? "https://geocode.maps.co/",
 });
 
 export default geoCodeBaseUrl;
